de08ceaedc9451647b19efa1157e6796f84c90a7
zahrablend <zahra@LAPTOP-010BK67M>
2023-09-13 13:26:23 +0300

    Update PrintWordsApp solution

--- src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package school.mjc.stage0.module3.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("java" + "\n is" + "\n smith");
+        System.out.println("java" + "\nis" + "\nsmith");
     }
 }
